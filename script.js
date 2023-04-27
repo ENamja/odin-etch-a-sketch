dd6de3eb1e640982dfa6076ab61f4ec16683903b
elisha <namelisha@gmail.com>
2023-04-26 23:22:40 -0700

    Added button that resets grid and creates a new one with different dimensions

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const GRID_ROWS = 16
-const GRID_COLUMNS = 16
+GRID_ROWS = 16
+GRID_COLUMNS = 16
 
-document.addEventListener("DOMContentLoaded", () => {
-
+function createGrid() {
     // Finds and stores the div with class container 
-    let container = document.querySelector("div.container")
+    let container = document.querySelector("div.container");
 
     // Creates width * length number of "grids" in the div with the given class of container
     for(let i = 0; i < GRID_ROWS; i++) {
@@ -16,7 +15,22 @@ document.addEventListener("DOMContentLoaded", () => {
             block.classList.add("block");
             row.appendChild(block);
         }
-    }   
+    }  
+}
+
+function deleteGrid() {
+    let blocks = document.querySelectorAll("div.block");
+    let rows = document.querySelectorAll("div.row");
+    blocks.forEach(block => {
+        block.remove();
+    })
+    rows.forEach(row => {
+        row.remove();
+    })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    createGrid();
 })
 
 document.addEventListener("mouseover", (event) => {
@@ -24,6 +38,24 @@ document.addEventListener("mouseover", (event) => {
         console.log(event.target.style);
     }
     else {
-        event.target.classList.add("hover");
+        if (event.target.classList.contains("block")) {
+            event.target.classList.add("hover");
+        }
     }
 })
+
+function newGrid() {
+    newDimensions = parseInt(prompt("Please enter the number of squares you would like per side in your new grid: "));
+    if (isNaN(newDimensions)) {
+        alert("Please enter only an integer when prompted.");
+        return;
+    }
+    if (newDimensions < 1 || newDimensions > 100) {
+        alert("Please enter an integer between 1 to 100.");
+        return;
+    }
+    GRID_ROWS = newDimensions;
+    GRID_COLUMNS = newDimensions;
+    deleteGrid();
+    createGrid();
+}
